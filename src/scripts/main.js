395c4d68bf7c0c982abae183aece4f9bc200a0cf
Felipe Martins Bonifacio <felipemartins.py@gmail.com>
2023-06-25 17:54:53 -0300

    Criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,38 @@
-console.log('Olá mundo')
+AOS.init();
+
+//Pegar o Mês e ano da maquina para o evento sempre estar em contagem regressiva
+const date = new Date()
+const month = date.getMonth() + 2;
+const year = date.getFullYear();
+
+const dataEvento = new Date(`${month} 10, ${year} 16:00:00`);
+
+//Alterar a data no header do evento
+document.getElementById('data').innerHTML=`10/${month}/${year}`;
+
+
+const TimeStampEvent = dataEvento.getTime();
+
+const CountTime = setInterval(function(){
+    const now = new Date();
+    const TimeStamp = now.getTime();
+
+    const DistanceToFinal = TimeStampEvent - TimeStamp;//ms
+
+    const DaysInMs = 1000 * 60 * 60 * 24;
+    const HoursInMs =  1000 * 60 * 60;
+    const MinsInsMs = 1000 * 60;
+
+    const Days = Math.floor(DistanceToFinal / DaysInMs);
+    const Hours = Math.floor(DistanceToFinal % DaysInMs / HoursInMs);
+    const Mins = Math.floor((DistanceToFinal % HoursInMs) / MinsInsMs);
+    const Seconds = Math.floor((DistanceToFinal % MinsInsMs) / 1000)
+
+    document.getElementById('contador').innerHTML=`${Days}d ${Hours}h ${Mins}m ${Seconds}s`;
+
+    if(DistanceToFinal < 0){
+        clearInterval(CountTime);
+        document.getElementById('contador').innerHTML="Evento Expirado"
+    }
+}, 1000)
+
